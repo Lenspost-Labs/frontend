bc4f2f497bd9603bdca618dafb99712be14dd5dc
Scriptscrypt <tirumaladasa@gmail.com>
2024-07-31 15:37:50 +0530

    Fix: New Logo

--- src/app/editor/sections/top-section/logo/Logo.jsx
+++ src/app/editor/sections/top-section/logo/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = ({ propWidth, propHeight }) => {
       <img
         width={propWidth}
         height={propHeight}
-        className="flex items-center justify-start object-contain h-20"
+        className="flex items-center justify-start object-contain px-2"
         src="/logo-trans-bg.svg"
         alt="lenspost"
       />

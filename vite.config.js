9560878e3cbc984601a9eff6b80484629ed8a6a8
aryan <agarwalaryan139@gmail.com>
2024-08-30 23:05:34 +0530

    feat: csp

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,39 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import csp from "vite-plugin-csp";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "lenspost-labs",
-    project: "lenspost-main-app"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "lenspost-labs",
+      project: "lenspost-main-app",
+    }),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+      },
+      protocolImports: true,
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: false,
+      },
+    },
+  },
+
+  server: {
+    hmr: false, // Disable HMR to avoid inline script injection
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; connect-src 'self' http://localhost:* https://*.dial.to/ https://proxy.dial.to/; img-src 'self' https://*.dial.to/ https://proxy.dial.to/; script-src 'self'; style-src 'self' 'unsafe-inline';",
+    },
+  },
+});

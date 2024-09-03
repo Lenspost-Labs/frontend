0a804a3f2b203eb1d16494cb00b39c0a34cf032d
Scriptscrypt <tirumaladasa@gmail.com>
2024-09-03 09:02:53 +0530

    Fix: DEV dscvr rollback

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,49 @@
+// Fix: DSCVR rollback
+
+// import { sentryVitePlugin } from "@sentry/vite-plugin";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     sentryVitePlugin({
+//       org: "lenspost-labs",
+//       project: "lenspost-main-app",
+//     }),
+//   ],
+
+//   build: {
+//     sourcemap: true,
+//     rollupOptions: {
+//       output: {
+//         inlineDynamicImports: false,
+//       },
+//     },
+//   },
+
+//   server: {
+//     hmr: false, // Disable HMR to avoid inline script injection
+//     headers: {
+//       "Content-Security-Policy":
+//         "default-src 'self'; connect-src 'self' http://localhost:* https://*.poster.fun/ https://app.poster.fun/; img-src 'self' https://*.poster.fun/ https://app.poster.fun/; script-src 'self'; style-src 'self' 'unsafe-inline';",
+//     },
+//   },
+// });
+
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    sentryVitePlugin({
-      org: "lenspost-labs",
-      project: "lenspost-main-app",
-    }),
-  ],
+  plugins: [react(), sentryVitePlugin({
+    org: "lenspost-labs",
+    project: "lenspost-main-app"
+  })],
 
   build: {
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: false,
-      },
-    },
-  },
-
-  server: {
-    hmr: false, // Disable HMR to avoid inline script injection
-    headers: {
-      "Content-Security-Policy":
-        "default-src 'self'; connect-src 'self' http://localhost:* https://*.poster.fun/ https://app.poster.fun/; img-src 'self' https://*.poster.fun/ https://app.poster.fun/; script-src 'self'; style-src 'self' 'unsafe-inline';",
-    },
-  },
-});
+    sourcemap: true
+  }
+})

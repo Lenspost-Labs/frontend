24ea2eef3f416849d04fcb239ef1d9b05965ee59
Scriptscrypt <tirumaladasa@gmail.com>
2024-09-06 20:56:58 +0530

    Fix: Added more urls

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
     hmr: false, // Disable HMR to avoid inline script injection
     headers: {
       "Content-Security-Policy":
-        "default-src 'self'; connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://api.polotno.com https://auth.privy.io https://o4506978044739584.ingest.us.sentry.io; img-src 'self' blob: data: https://ipfs.dscvr.one https://media.dscvr.one https://media1.stg.dscvr.one https://fal.media; script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline' https://eu.posthog.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; style-src-elem 'self' https://unpkg.com; font-src 'self' https://fonts.gstatic.com;",
+        "default-src 'self'; connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://api.polotno.com https://auth.privy.io https://o4506978044739584.ingest.us.sentry.io; img-src 'self' blob: data: https://ipfs.dscvr.one https://media.dscvr.one https://media1.stg.dscvr.one https://fal.media; script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline' https://eu.posthog.com https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; style-src-elem 'self' https://unpkg.com; font-src 'self' https://fonts.gstatic.com;",
     },
   },
 });

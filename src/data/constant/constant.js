2113ff94a01a6e63bf817e76bcc60112b78edd1b
MrPreet <josonpreet@gmail.com>
2025-02-12 17:59:18 +0530

    fix: issue

--- src/data/constant/constant.js
+++ src/data/constant/constant.js
@@ -15,7 +15,7 @@ export const APP_URL = 'https://app.poster.fun'
 
 export const APP_LENS_HANDLE = '@lenspost'
 export const APP_SOLANA_ADDRESS = '2PsV6hNEUc3rSMGqKcHTnRBemaWBQX3dYgUqVtEFxkwa'
-export const APP_ETH_ADDRESS = '0x77fAD8D0FcfD481dAf98D0D156970A281e66761b'
+export const APP_ETH_ADDRESS = '0xc5b436A62f7d0A5412f60C74BD1dac3ff404eB2a' //'0x77fAD8D0FcfD481dAf98D0D156970A281e66761b'
 
 export const FREE_MINTS = 10
 export const FRAME_URL = FRAME_URLS[ENVIRONMENT]

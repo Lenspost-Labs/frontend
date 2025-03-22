8769151db5bdb6a50454ad55a121e4fbe17b4ace
Pankaj Kumar <pankajkumardas727@gmail.com>
2025-03-22 12:21:07 +0530

    feat: update camp network

--- src/data/constant/enabledChain.js
+++ src/data/constant/enabledChain.js
@@ -2,6 +2,7 @@ import { arbitrum, base, polygon, morph } from "viem/chains";
 import { ham } from "../network/ham";
 import { og } from "../network/og";
 import { campNetworkTestnetV2 } from "../network/campNetworkTestnetV2";
+import { basecampTestnet } from "../network/basecampTestnet";
 
 export const LENSPOST_721_ENALBED_CHAINS = [
   base,
@@ -11,4 +12,5 @@ export const LENSPOST_721_ENALBED_CHAINS = [
   ham,
   morph,
   campNetworkTestnetV2,
+  basecampTestnet,
 ];

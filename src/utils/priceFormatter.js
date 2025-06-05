48e9e2f2cc9dcb4c83b156e844d8a19563f34643
Pankaj Kumar <pankajkumardas727@gmail.com>
2025-06-05 14:54:54 +0530

    fix: decimal fix

--- src/utils/priceFormatter.js
+++ src/utils/priceFormatter.js
@@ -1,13 +1,12 @@
-import { arbitrum } from "viem/chains";
-
-const MULTIPLIER_ARBITRUM = 10 ** 6;
 const MULTIPLIER_DEFAULT = 10 ** 18;
 
-export const priceFormatter = (chainId, price) => {
-  const { id: arbitrumId } = arbitrum || {};
+const MULTIPLIER_MAP = {
+  USDC: 10 ** 6,
+  USDT: 10 ** 6,
+};
 
-  const multiplier =
-    chainId === arbitrumId ? MULTIPLIER_ARBITRUM : MULTIPLIER_DEFAULT;
+export const priceFormatter = (tokenSymbol, price) => {
+  const multiplier = MULTIPLIER_MAP[tokenSymbol] || MULTIPLIER_DEFAULT;
 
   return Number(price) * multiplier;
 };
